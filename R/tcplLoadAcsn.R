1d338d2c202cdd3b4a8e84d7199f5737d21b4804
paulsonak <akpaulson547@gmail.com>
2020-08-06 14:45:05 -0700

    new function to list assay component source names

---
+++ R/tcplLoadAcsn.R
@@ -0,0 +1,27 @@
+#-------------------------------------------------------------------------------
+# tcplLoadAcsn: Load assay component source name by acid
+#-------------------------------------------------------------------------------
+
+#' @rdname assay_funcs
+#' @import data.table
+#' @export
+
+tcplLoadAcsn <- function(fld = NULL, val = NULL, add.fld = NULL) {
+  tbl = c("assay_component", "assay_component_map", "assay")
+  out <- c("assay_component.acid",
+           "assay_component.assay_component_name"
+           "assay_component_map.acsn")
+
+  qstring <- .buildAssayQ(out = out,
+                          tblo = c(1, 2, 4, 6, 3),
+                          fld = fld,
+                          val = val,
+                          add.fld = add.fld)
+
+  dat <- tcplQuery(query = qstring, db = getOption("TCPL_DB"), tbl=tbl)
+
+  dat[]
+
+}
+
+#-------------------------------------------------------------------------------
