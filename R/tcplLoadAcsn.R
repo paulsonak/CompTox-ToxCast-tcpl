fbe84474fb408ab4c383b9bee83c66b5b8980a85
paulsonak <akpaulson547@gmail.com>
2020-08-06 14:48:54 -0700

    fixed typo in new acsn function

--- R/tcplLoadAcsn.R
+++ R/tcplLoadAcsn.R
@@ -9,7 +9,7 @@
 tcplLoadAcsn <- function(fld = NULL, val = NULL, add.fld = NULL) {
   tbl = c("assay_component", "assay_component_map", "assay")
   out <- c("assay_component.acid",
-           "assay_component.assay_component_name"
+           "assay_component.assay_component_name",
            "assay_component_map.acsn")
 
   qstring <- .buildAssayQ(out = out,
