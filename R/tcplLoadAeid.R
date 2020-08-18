46d354efb7b33090ee8ff7097cfcae67724ca2af
paulsonak <akpaulson547@gmail.com>
2020-08-18 13:21:07 -0700

    translate spids from int to string

--- R/tcplLoadAeid.R
+++ R/tcplLoadAeid.R
@@ -8,17 +8,17 @@
 
 tcplLoadAeid <- function(fld = NULL, val = NULL, add.fld = NULL) {
   tbl = c("assay_component_endpoint", "assay", "assay_component")
-  out <- c("assay_component_endpoint.aeid", 
+  out <- c("assay_component_endpoint.aeid",
            "assay_component_endpoint.assay_component_endpoint_name")
-  
-  qstring <- .buildAssayQ(out = out, 
-                          tblo = c(1, 2, 4, 3, 6), 
-                          fld = fld, 
-                          val = val, 
+
+  qstring <- .buildAssayQ(out = out,
+                          tblo = c(1, 2, 4, 3, 6),
+                          fld = fld,
+                          val = val,
                           add.fld = add.fld)
-  
+
   dat <- tcplQuery(query = qstring, db = getOption("TCPL_DB"), tbl=tbl)
-  
+
   dat[]
 
 }
